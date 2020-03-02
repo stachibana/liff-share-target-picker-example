ace682a7211f00cf051e83e6a005ebb368f2b5d6
Tachibana Sho <JP21988@w022511902019m-7875.local>
2020-03-02 23:01:43 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template, abort, redirect, jsonify
+from flask_bootstrap import Bootstrap
+import uuid
+import os
+from os.path import join, dirname
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def do_get():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
